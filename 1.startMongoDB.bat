bfcffa6a299b8615e8ead1bf23d358eedb021b4b
Naveen <naveen.upbeat@gmail.com>
2015-08-06 14:00:38 -0700

    StartMongoDB bat file update
    Updated the batch file to check if the 'data' folder exists otherwise
    create one.
    
    Used by mongodb to run.

--- 1.startMongoDB.bat
+++ 1.startMongoDB.bat
@@ -1,2 +1,3 @@
+IF exist "..\..\0.DevEnv\mongodb\3\data" ( echo "..\..\0.DevEnv\mongodb\3\data" exists ) ELSE ( mkdir "..\..\0.DevEnv\mongodb\3\data" && echo "..\..\0.DevEnv\mongodb\3\data" created)
 start /min ..\..\0.DevEnv\mongodb\3\bin\mongod --dbpath ..\..\0.DevEnv\mongodb\3\data
 exit
