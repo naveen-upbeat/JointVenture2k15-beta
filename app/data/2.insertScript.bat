9bc0551f66a74c4bee851d8fa993a78d4d6e1631
Naveen <naveen.upbeat@gmail.com>
2015-08-25 01:35:02 -0700

    Updated Static Data insert
    using mongoimport instead of inline

--- app/data/2.insertScript.bat
+++ app/data/2.insertScript.bat
@@ -1,3 +1,4 @@
 cd 2.insert
-FOR %%c in (*.*) DO  cmd /c ..\2.insertScriptExt.bat %%c
+FOR %%c in (*.*) DO copy %%c %%c.json &  cmd /c ..\..\..\..\..\0.DevEnv\mongodb\3\bin\mongoimport /h localhost:27017  /d jvdb /c %%c /file %%c.json
+FOR %%c in (*.json) DO del %%c
 cd ..
